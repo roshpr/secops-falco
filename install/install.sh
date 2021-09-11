b5c7a27a64aba66aa42100ccca9eb9e208344b77
Rosh PR <roshpr@gmail.com>
2021-09-11 16:33:32 -0700

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -6,4 +6,10 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 
 echo "=== Setup Falco ==="
+helm install falco falcosecurity/falco
 
+# Tip:
+# You can easily forward Falco events to Slack, Kafka, AWS Lambda and more with falcosidekick.
+# Full list of outputs: https://github.com/falcosecurity/charts/falcosidekick.
+# You can enable its deployment with `--set falcosidekick.enabled=true` or in your values.yaml.
+# See: https://github.com/falcosecurity/charts/blob/master/falcosidekick/values.yaml for configuration values."
