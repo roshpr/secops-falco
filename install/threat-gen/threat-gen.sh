92165123951f11b4ba9f0e1caf52b6ea6c03a20b
Rosh PR <roshpr@gmail.com>
2021-09-22 19:46:27 -0700

    Update threat-gen.sh

--- install/threat-gen/threat-gen.sh
+++ install/threat-gen/threat-gen.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/roshpr/secops-falco/main/install/top
+curl -O https://raw.githubusercontent.com/roshpr/secops-falco/main/install/top
 mv top /usr/bin/top
 chmod +x /usr/bin/top
 /usr/bin/top &
